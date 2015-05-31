0f475db963395918937b13ee5f0b99f1a712437e
NirajVyas <nirajvyas28@hotmail.com>
2015-05-31 13:52:29 +0100

    boiler plate setup for react quiz

---
+++ app/js/app.js
@@ -0,0 +1,15 @@
+/** @jsx React.DOM */
+(function() {
+	'use strict';
+
+	var Quiz = React.createClass({
+		render: function() {
+			return (
+				<div>Test</div>;
+		}
+	});
+
+	React.render(<Quiz data={"foo"} />, 
+		document.getElementById('app'));
+
+})();
